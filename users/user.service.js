d3f8b4c60a9dabccb6ffde9eca8f4f691f7a4183
Ivan Megias <ivan92.md@gmail.com>
2022-02-11 15:59:17 +0100

    feat: add the basic auth feature

---
+++ users/user.service.js
@@ -0,0 +1,44 @@
+﻿// users hardcoded for simplicity
+const users = [
+  {
+    id: 1,
+    email: "ivan@gmail.com",
+    password: "test1234",
+    firstName: "Test",
+    lastName: "User",
+  },
+  {
+    id: 2,
+    email: "diego@gmail.com",
+    password: "test1234",
+    firstName: "TestD",
+    lastName: "UserD",
+  },
+  {
+    id: 3,
+    email: "mateen@gmail.com",
+    password: "test1234",
+    firstName: "TestM",
+    lastName: "UserM",
+  },
+];
+
+module.exports = {
+  authenticate,
+  getAll,
+};
+
+async function authenticate({ email, password }) {
+  const user = users.find((u) => u.email === email && u.password === password);
+  if (user) {
+    const { password, ...userWithoutPassword } = user;
+    return userWithoutPassword;
+  }
+}
+
+async function getAll() {
+  return users.map((u) => {
+    const { password, ...userWithoutPassword } = u;
+    return userWithoutPassword;
+  });
+}
